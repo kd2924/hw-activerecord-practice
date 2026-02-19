f5ed50cdb399fd54473e3ba1c44d22beb8421b3f
kd2924 <kd2924@nyu.edu>
2026-02-18 21:44:43 -0500

    Complete Activerecord Chip

--- lib/activerecord_practice.rb
+++ lib/activerecord_practice.rb
@@ -23,10 +23,57 @@ class Customer < ApplicationRecord
   def self.any_candice
     # YOUR CODE HERE to return all customer(s) whose first name is Candice
     # probably something like:  Customer.where(....)
+    where(first: 'Candice')
   end
 
   def self.with_valid_email
+    where("email LIKE ?", "%@%")
     # YOUR CODE HERE to return only customers with valid email addresses (containing '@')
   end
   # etc. - see README.md for more details
+  def self.with_dot_org_email
+    where("email LIKE '%.org'")
+  end
+
+  def self.with_invalid_email
+    where("email != '' AND email IS NOT NULL AND email NOT LIKE?", "%@%")
+  end
+  
+  def self.with_blank_email
+    where(email: [nil,''])
+  end
+
+  def self.born_before_1980
+    where('birthdate < ?', Time.parse('1980-01-1'))
+  end
+
+  def self.last_names_starting_with_b
+    where("last LIKE?", "B%").order(:birthdate)
+  end
+
+  def self.twenty_youngest
+    order(birthdate: :desc).limit(20)
+  end
+
+  def self.update_gussie_murray_birthdate
+    where(first: 'Gussie', last: 'Murray')
+      .update_all(birthdate: Time.parse('February 8, 2004'))
+  end
+
+  def self.change_all_invalid_emails_to_blank
+    with_invalid_email.update_all(email: '')
+  end
+
+  def self.delete_meggie_herman
+    where(first:"Meggie", last:"Herman").delete_all
+  end
+
+  def self.delete_everyone_born_before_1978
+    where('birthdate < ?', Time.zone.parse('1 January 1978')).delete_all
+  end
+
+  def self.with_valid_email_and_born_before_1980
+    with_valid_email.where("birthdate < ?", Time.parse("1980-01-01"))
+  end
+
 end
